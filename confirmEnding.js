0d8f816af3a2e9f969c9ea49077072223801ef1d
ku0100 <kuwilliams1@gmail.com>
2017-05-31 15:10:18 -0400

    actually followed the instructions and used substring() this go-round

--- confirmEnding.js
+++ confirmEnding.js
@@ -1,9 +1,9 @@
 function confirmEnding(str, target) {
   // "Never give up and good luck will find you."
   // -- Falcor
-  str = str.toLowerCase()
-  var firstLetter = str[str.length - 1];
-  if (firstLetter === target) {
+  str = str.toLowerCase();
+  tgtStr = str.substr((str.length - target.length), target.length);
+  if (tgtStr === target) {
     return true;
   } else {
     return false;
