298b4a9fbb6f294c8a1eef6644e1b13d378ef5e7
ku0100 <kuwilliams1@gmail.com>
2017-05-31 14:43:04 -0400

    added commentary throughout

--- returnLargestNumber.js
+++ returnLargestNumber.js
@@ -1,18 +1,16 @@
 function largestOfFour(arr) {
   // You can do this!
-  var largestArr = [];
-
-
-  for (i = 0; i < arr.length; i++) {
+  var largestArr = []; // intialize a blank array that max values will be pushed to
+  for (i = 0; i < arr.length; i++) { // loop over every array in the provided array
     var currentMax = 0;
-    for (j = 0; j < arr[i].length; j++) {
-      if (arr[i][j] > currentMax) {
-          currentMax = arr[i][j];
+    for (j = 0; j < arr[i].length; j++) { // for each array within an array, loop over each value
+      if (arr[i][j] > currentMax) { // if the looped value is larger than the current max (initialized to 0)
+          currentMax = arr[i][j]; // set new max
         } else {
-          currentMax = currentMax;
+          currentMax = currentMax; // otherwise the max stays the same
         };
       };
-      largestArr.push(currentMax);
+      largestArr.push(currentMax); // after the array within an array has been looped through, push that array's current max
   };
   return largestArr;
 }
