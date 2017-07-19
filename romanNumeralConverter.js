222dba40dce87097350856f37a343731f33a0b3e
ku0100 <kuwilliams1@gmail.com>
2017-07-19 15:18:27 -0400

    cleaned it up significantly by using a dictionary instead of the sloppy setup

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -1,55 +1,31 @@
 function convertToRoman(num) {
+  var dictionary = {
+    "M": 1000,
+    "CM": 900,
+    "D": 500,
+    "CD": 400,
+    "C": 100,
+    "XC": 90,
+    "L": 50,
+    "XL": 40,
+    "X": 10,
+    "IX": 9,
+    "V": 5,
+    "IV": 4,
+    "I": 1,
+  }
+
   var romanNumerals = "";
   while (num > 0) {
-    if (num >= 1000) {
-      var m = Math.floor(num / 1000);
-      num = (num % 1000);
-      romanNumerals = "M".repeat(m);
-    } else if (num >= 900) {
-      num = num - 900;
-      romanNumerals = romanNumerals + "CM";
-    } else if (num >= 500) {
-      var d = Math.floor(num / 500);
-      num = (num % 500);
-      romanNumerals = romanNumerals + "D".repeat(d);
-    } else if (num >= 400) {
-      num = num - 400;
-      romanNumerals = romanNumerals + "CD";
-    } else if (num >= 100) {
-      var c = Math.floor(num / 100);
-      num = (num % 100);
-      romanNumerals = romanNumerals + "C".repeat(c);
-    } else if (num >= 90) {
-      num = num - 90;
-      romanNumerals = romanNumerals + "XC";
-    } else if (num >= 50) {
-      var l = Math.floor(num / 50);
-      num = (num % 50);
-      romanNumerals = romanNumerals + "L".repeat(l);
-    } else if (num >= 40) {
-      num = num - 40;
-      romanNumerals = romanNumerals + "XL";
-    } else if (num >= 10) {
-      var x = Math.floor(num / 10);
-      num = (num % 10);
-      romanNumerals = romanNumerals + "X".repeat(x);
-    } else if (num >= 9) {
-      num = num - 9;
-      romanNumerals = romanNumerals + "IX";
-    } else if (num >= 5) {
-      var v = Math.floor(num / 5);
-      num = (num % 5);
-      romanNumerals = romanNumerals + "V".repeat(v);
-    } else if (num >= 4) {
-      num = num - 4;
-      romanNumerals = romanNumerals + "IV";
-    } else {
-      var i = Math.floor(num / 1);
-      num = (num % 1);
-      romanNumerals = romanNumerals + "I".repeat(i);
+    for (var key in dictionary) {
+      if (num / dictionary[key] >= 1) {
+        var x = Math.floor((num / dictionary[key]));
+        romanNumerals = romanNumerals + key.repeat(x);
+        num = (num - (x * dictionary[key]));
+      }
     }
+    return romanNumerals;
   }
-  return romanNumerals;
 }
 
-console.log(convertToRoman(44));
+console.log(convertToRoman(4453));
