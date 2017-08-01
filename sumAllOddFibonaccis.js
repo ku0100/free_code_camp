c7d82cfd5cb82df0ac4a34e10cb1c0139b9697e2
ku0100 <kuwilliams1@gmail.com>
2017-08-01 08:38:51 -0400

    sumAllOddFibonaccis
    updated code to work, needed to rearrange some elements

--- sumAllOddFibonaccis.js
+++ sumAllOddFibonaccis.js
@@ -3,28 +3,16 @@ function sumFibs(num) {
     var sum = 0;
     var fibTotal = 0;
 
-    while (firstNum < num) {
-        sum = sum + firstNum;
-        firstNum = sum;
+    while (firstNum <= num) {
         if (firstNum % 2 !== 0) {
-            fibTotal = fibTotal + firstNum;
-            continue;
+            fibTotal += firstNum
         }
+
+        firstNum += sum;
+        sum = firstNum - sum;
     }
 
     return fibTotal;
-
-
-
-
-    // for (var i = 0; i < num; i++) {
-    //     sum = sum + firstNum;
-    //     firstNum = sum;
-    //     if (firstNum % 2 !== 0) {
-    //         fibTotal = fibTotal + firstNum;
-    //     }
-    // }
-    // return fibTotal;
 }
 
-console.log(sumFibs(1000));
+console.log(sumFibs(3));
